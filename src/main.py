2d3d89c6bda083bad9b257403bba00e7846535b9
Patrick Andrew Hartnett <phartnett@sdl.com>
2024-11-24 15:56:37 +0100

    updated version to 1.0.2

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from api import router
 app = FastAPI(
     title="Hugging Face Transformers Service",
     description="This is a FastAPI application designed to provide an intuitive and efficient interface for working with Hugging Face models, specifically catering to translation and text generation tasks. The service allows users to **download and mount** models locally, making it possible to run model inference without requiring an internet connection once the models are downloaded.",
-    version="1.0.1"
+    version="1.0.2"
 )
 
 app.include_router(router)
