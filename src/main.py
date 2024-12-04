ce4637173074bb21b60041f92ce1085fb586578c
Patrick Andrew Hartnett <phartnett@sdl.com>
2024-12-04 10:21:05 +0100

    bump version to 1.0.5

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from api import router
 app = FastAPI(
     title="Hugging Face Transformers Service",
     description="This **Local LLM server** application is a Windows service application designed to provide an intuitive and efficient interface for working with Hugging Face models, specifically catering to translation and text generation tasks.",
-    version="1.0.4"
+    version="1.0.5"
 )
 
 app.include_router(router)
